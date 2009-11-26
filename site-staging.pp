8772f17245c3d1bb362e27a793d6cee1a7ce6b8e
Ben Hearsum <bhearsum@mozilla.com>
2009-11-26 16:06:54 -0500

    bug 429430: Redesign mac refimage so no additional manual setup needed - disable mac key management. r=catlee

--- site-staging.pp
+++ site-staging.pp
@@ -29,23 +29,23 @@ node "try-linux-slave05.mozilla.org" {
 }
 
 node "moz2-darwin9-slave03.build.mozilla.org" {
-    include base, staging-buildslave, build-network, mac-build-network-staging-keys
+    include base, staging-buildslave, build-network
 }
 
 node "moz2-darwin9-slave04.build.mozilla.org" {
-    include base, staging-buildslave, build-network, mac-build-network-staging-keys
+    include base, staging-buildslave, build-network
 }
 
 node "moz2-darwin9-slave08.build.mozilla.org" {
-    include base, staging-buildslave, build-network, mac-build-network-staging-keys
+    include base, staging-buildslave, build-network
 }
 
 node "bm-xserve14.build.mozilla.org" {
-    include base, staging-buildslave, build-network, mac-build-network-staging-keys
+    include base, staging-buildslave, build-network
 }
 
 node "try-mac-slave05.mozilla.org" {
-    include base, staging-buildslave, sandbox-network, mac-staging-try-keys
+    include base, staging-buildslave, sandbox-network
 }
 
 node default {
