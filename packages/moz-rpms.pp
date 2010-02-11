c3a8dc5a1547ac05b76cf6af8f748809c268a854
Chris AtLee <catlee@mozilla.com>
2010-02-11 09:55:21 -0500

    Bug 496901 - Deploy ccache on linux. r=bhearsum

---
+++ packages/moz-rpms.pp
@@ -0,0 +1,19 @@
+# moz-rpms.pp
+# moz-rpms rpm packages
+
+class moz-rpms {
+    Package{ provider => rpm, ensure => installed }
+
+    package {
+        "ccache":
+            source => "/N/centos5/moz-rpms/ccache-2.4-99.11.i386.rpm";
+    }
+    exec {
+        "/usr/bin/ccache -M 2G":
+            environment => ["CCACHE_DIR=/builds/ccache"],
+            refreshonly => true,
+            user => "cltbld",
+            group => "cltbld",
+            subscribe => package["ccache"];
+    }
+}
