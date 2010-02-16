74b4af32bb6d534ff65b3e57e27eb04ef5eb9523
Ben Hearsum <bhearsum@mozilla.com>
2010-02-16 17:06:21 -0500

    Merge ccache stuff into 64-bit linux manifests.

--- packages/moz-rpms.pp
+++ packages/moz-rpms.pp
@@ -4,24 +4,30 @@
 class moz-rpms {
     Package{ provider => rpm, ensure => installed }
 
-    package {
-        "ccache":
-            source => "/N/centos5/moz-rpms/ccache-2.4-99.11.i386.rpm";
-    }
-    exec {
-        "/usr/bin/ccache -M 2G":
-            environment => ["CCACHE_DIR=/builds/ccache"],
-            refreshonly => true,
-            user => "cltbld",
-            group => "cltbld",
-            subscribe => file["/builds/ccache"],
-            require => package["ccache"];
-    }
-    file {
-        "/builds/ccache":
-            owner => "cltbld",
-            group => "cltbld",
-            ensure => directory,
-            require => mount["/builds"];
+    case $hardwaremodel {
+        "x86_64": {
+        }
+        default: {
+            package {
+                "ccache":
+                    source => "/N/centos5/moz-rpms/ccache-2.4-99.11.i386.rpm";
+            }
+            exec {
+                "/usr/bin/ccache -M 2G":
+                    environment => ["CCACHE_DIR=/builds/ccache"],
+                    refreshonly => true,
+                    user => "cltbld",
+                    group => "cltbld",
+                    subscribe => file["/builds/ccache"],
+                    require => package["ccache"];
+            }
+            file {
+                "/builds/ccache":
+                    owner => "cltbld",
+                    group => "cltbld",
+                    ensure => directory,
+                    require => mount["/builds"];
+            }
+        }
     }
 }
