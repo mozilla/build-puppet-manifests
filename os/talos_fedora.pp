8c35dc6e2c3087b50cd03b9816de3d8358649d79
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2010-04-21 10:40:57 -0400

    Bug 558521. Update gtk on talos fedora slaves. r=bhearsum

--- os/talos_fedora.pp
+++ os/talos_fedora.pp
@@ -6,5 +6,19 @@ class talos_fedora {
     package {
         "mercurial":
             source => "${httproot}/talos/linux/RPMS/${hardwaremodel}/mercurial-1.5.1-1.fc12.${hardwaremodel}.rpm";
+        
+    }
+    
+    case $hardwaremodel {
+        "i686": {
+            package {
+                "gtk2-immodule-xim":
+                    ensure => absent,
+                    before => Package["gtk2"];
+                "gtk2":
+                    source => "${httproot}/talos/linux/RPMS/${hardwaremodel}/gtk2-2.18.9-3.fc12.${hardwaremodel}.rpm",
+                    ensure => latest;
+            }
+        }
     }
 }
