b6c72097134e5c07c998a2e52f3d88945b785174
Rail Aliev <none@none>
2010-06-01 11:13:48 -0400

    bug 566275: Install ccache on the linux64 build slaves - Puppet manifest for 64 bit ccache. r=bhearsum

--- packages/moz-rpms.pp
+++ packages/moz-rpms.pp
@@ -6,36 +6,32 @@ class moz-rpms {
 
     case $hardwaremodel {
         "x86_64": {
-            package {
-                "yasm":
-                    source => "${platform_httproot}/RPMs/yasm-1.0.1-1.x86_64.rpm";
-            }
+            $rpm_arch = "x86_64"
         }
         default: {
-            package {
-                "ccache":
-                    source => "${platform_httproot}/RPMs/ccache-2.4-99.11.i386.rpm";
-            }
-            package {
-                "yasm":
-                    source => "${platform_httproot}/RPMs/yasm-1.0.1-1.i386.rpm";
-            }
-            exec {
-                "/usr/bin/ccache -M 2G":
-                    environment => ["CCACHE_DIR=/builds/ccache"],
-                    refreshonly => true,
-                    user => "cltbld",
-                    group => "cltbld",
-                    subscribe => File["/builds/ccache"],
-                    require => Package["ccache"];
-            }
-            file {
-                "/builds/ccache":
-                    owner => "cltbld",
-                    group => "cltbld",
-                    ensure => directory,
-                    require => Mount["/builds"];
-            }
+            $rpm_arch = "i386"
         }
+    }
+    package {
+        "yasm":
+            source => "${platform_httproot}/RPMs/yasm-1.0.1-1.$rpm_arch.rpm";
+        "ccache":
+            source => "${platform_httproot}/RPMs/ccache-2.4-99.11.$rpm_arch.rpm";
+    }
+    exec {
+        "/usr/bin/ccache -M 2G":
+            environment => ["CCACHE_DIR=/builds/ccache"],
+            refreshonly => true,
+            user => "cltbld",
+            group => "cltbld",
+            subscribe => File["/builds/ccache"],
+            require => Package["ccache"];
+    }
+    file {
+        "/builds/ccache":
+            owner => "cltbld",
+            group => "cltbld",
+            ensure => directory,
+            require => Mount["/builds"];
     }
 }
