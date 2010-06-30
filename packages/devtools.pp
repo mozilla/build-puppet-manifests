06dfffb61078b4bfba9eb5692f4a2921eeeaa23c
Mike Taylor <mtaylor@mozilla.com>
2010-06-30 13:03:31 -0400

    bug 574477: Need sys/queue.h in the NDK to support IPC enabled Android builds - point centos5 puppet manifests to new android-ndk-r4c rpm. r=bhearsum

--- packages/devtools.pp
+++ packages/devtools.pp
@@ -153,7 +153,7 @@ class devtools {
                             subscribe   => File["/tools/android-sdk"];
                         "android-ndk":
                             creates     => "/tools/android-ndk-r4c/build/tools/make-release.sh",
-                            version     => "r4c-0moz1",
+                            version     => "r4c-0moz2",
                             subscribe   => File["/tools/android-ndk"];
                         "mercurial":
                             creates     => "/tools/python-2.5.1/lib/python2.5/site-packages/mercurial/version.py",
