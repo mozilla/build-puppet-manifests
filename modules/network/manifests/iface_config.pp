bbf7508f98878858f1fdcb458512292e90fae3ab
Dustin J. Mitchell <dustin@mozilla.com>
2011-03-02 11:46:37 -0600

    Bug 636069: make Fedora hosts more resilient to DHCP downtime

---
+++ modules/network/manifests/iface_config.pp
@@ -0,0 +1,35 @@
+# Make sure the system's interfaces are configured correctly.  This
+# usually amounts to making sure they talk to DHCP.
+class network::iface_config {
+    case $operatingsystem {
+        Fedora: {
+
+            # on Fedora systems (regardless of bitlength), we need to inject
+            # the PERSISTENT_DHCLIENT=yes argument into the interface
+            # configuration script, or the slaves will give up on DHCP too
+            # quickly - see bug 636051 and bug 636069.  Since fedora invents a
+            # new ethN interface for every hardware address it sees, we
+            # configure this for both eth0, and blow away the file where Fedora
+            # "remembers" MAC addresses.  The net effect is that most of the
+            # time, the machine will come up with eth0 as the active interface.
+            # In cases where eth1 is the active interface, the default behavior
+            # is to use DHCP anyway, although without PERSISTENT_DHCLIENT, but
+            # this is good enough.
+
+            file {
+                "/etc/sysconfig/network-scripts/ifcfg-eth0":
+                    source => "puppet:///network/fedora-ifcfg-eth0";
+
+                "/etc/udev/rules.d/70-persistent-net.rules":
+                    content => "# clobbered on startup by puppet";
+
+                    # (note that fedora will append to this file on every boot,
+                    # so this resource will actuate on every puppet run)
+            }
+        }
+
+        # other operating systems seem to work OK based on the refimage; add
+        # more here when that proves untrue
+
+    }
+}
---
+++ modules/network/manifests/init.pp
@@ -0,0 +1,5 @@
+# This class does what it takes to make sure the system's networking is set up
+# correctly.
+class network {
+    include network::iface_config
+}
