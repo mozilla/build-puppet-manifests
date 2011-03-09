689e0e2a3b704f096afe6b9064d16bb51a6d81d8
John Ford <jhford@mozilla.com>
2011-03-09 11:30:39 -0800

    Specify different mount points for linux64-ix-* r=bhearsum

--- classes/ix.pp
+++ classes/ix.pp
@@ -1,13 +1,29 @@
 class ix {
-    mount {
-        "builds":
-            name   => "/builds",
-            atboot => true,
-            device => "/dev/sda4",
-            ensure => "mounted",
-            fstype => "ext3",
-            options => "noatime",
-            remounts => true;
+    case $hardwaremodel {
+        "x86_64": {
+            mount {
+                "builds":
+                    name   => "/builds",
+                    atboot => true,
+                    device => "/dev/mapper/vg_linux64-lv_builds",
+                    ensure => "mounted",
+                    fstype => "ext3",
+                    options => "noatime",
+                    remounts => true;
+            }
+        }
+        default: {
+            mount {
+                "builds":
+                    name   => "/builds",
+                    atboot => true,
+                    device => "/dev/sda4",
+                    ensure => "mounted",
+                    fstype => "ext3",
+                    options => "noatime",
+                    remounts => true;
+            }
+        }
     }
     file {
         "/dev/ptmx":
