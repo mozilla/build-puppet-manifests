bf3c81769c55ed71626d32e589562edf828106f3
Chris AtLee <catlee@mozilla.com>
2011-07-27 14:30:54 -0400

    Bug 673657: Update clang version to r135723. r=bhearsum

--- modules/packages/manifests/devtools.pp
+++ modules/packages/manifests/devtools.pp
@@ -36,7 +36,7 @@ class packages::devtools {
             }
             install_rpm {
                 "clang":
-                    version => "3.0-r135210.moz0",
+                    version => "3.0-r135723.moz0",
                     creates => "/tools/clang-3.0/bin/clang";
                 "moz_binutils_2.21.1":
                     version => "2.21.1-1",
@@ -254,10 +254,10 @@ class packages::devtools {
                     onlyif => "/bin/test -f /opt/local/bin/hg";
             }
             package {
-                "clang-3.0-r135210.moz0.dmg":
+                "clang-3.0-r135723.moz0.dmg":
                     provider    => pkgdmg,
                     ensure      => installed,
-                    source      => "${platform_httproot}/DMGs/clang-3.0-r135210.moz0.dmg";
+                    source      => "${platform_httproot}/DMGs/clang-3.0-r135723.moz0.dmg";
             }
             install_dmg {
                 "Twisted-8.0.1.dmg":
