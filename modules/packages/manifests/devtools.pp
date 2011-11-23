54d5a21c1423e7fc121f22cdcd0124a099f30612
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-11-23 14:03:16 -0500

    Bug 704420 - Upgrade clang on the slaves. r=rail.

--- modules/packages/manifests/devtools.pp
+++ modules/packages/manifests/devtools.pp
@@ -38,7 +38,7 @@ class packages::devtools {
             }
             install_rpm {
                 "clang":
-                    version => "3.0-r144446.moz0",
+                    version => "3.0-r145057.moz0",
                     creates => "/tools/clang-3.0/bin/clang";
                 "moz_binutils_2.21.1":
                     version => "2.21.1-1",
@@ -266,10 +266,10 @@ class packages::devtools {
                     onlyif => "/bin/test -f /opt/local/bin/hg";
             }
             package {
-                "clang-3.0-r144446.moz0.dmg":
+                "clang-3.0-r145057.moz0.dmg":
                     provider    => pkgdmg,
                     ensure      => installed,
-                    source      => "${platform_httproot}/DMGs/clang-3.0-r144446.moz0.dmg";
+                    source      => "${platform_httproot}/DMGs/clang-3.0-r145057.moz0.dmg";
             }
             install_dmg {
                 "Twisted-8.0.1.dmg":
