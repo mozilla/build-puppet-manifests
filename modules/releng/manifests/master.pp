047841b8971071a52311bd910526104ca9614231
Ben Hearsum <bhearsum@mozilla.com>
2012-03-28 14:20:33 -0400

    Add missing comma =/

--- modules/releng/manifests/master.pp
+++ modules/releng/manifests/master.pp
@@ -17,7 +17,7 @@ class releng::master {
             include ganglia::client
             package {
                 "epel-release":
-                    source => "http://mrepo.mozilla.org/mrepo/5-x86_64/RPMS.epel/epel-release-5-4.noarch.rpm"
+                    source => "http://mrepo.mozilla.org/mrepo/5-x86_64/RPMS.epel/epel-release-5-4.noarch.rpm",
                     provider => "rpm";
                 # So that puppet help works
                 "ruby-rdoc":
