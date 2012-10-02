1ce1e73f47936d0218522e0ca81fb5c79044d195
Aki Sasaki <asasaki@mozilla.com>
2012-10-02 11:02:22 -0700

    backing out 2c1d4cf15c43

--- os/talos_fedora.pp
+++ os/talos_fedora.pp
@@ -21,73 +21,7 @@ class talos_fedora {
             source => "${platform_httproot}/RPMs/python-devel-2.6.2-2.fc12.${hardwaremodel}.rpm",
             ensure => '2.6.2-2.fc12';
     }
-
-    # bug 789651 mesa-libGL-devel for b2g emulator testing
-    case $hardwaremodel {
-        'i686': {
-            package {
-                "kernel-headers":
-                    source => "${platform_httproot}/RPMs/kernel-headers-2.6.31.5-127.fc12.${hardwaremodel}.rpm",
-                    ensure => "2.6.31.5-127.fc12";
-                "pkgconfig":
-                    source => "${platform_httproot}/RPMs/pkgconfig-0.23-9.fc12.${hardwaremodel}.rpm",
-                    ensure => "0.23-9.fc12";
-                "libX11-devel":
-                    source => "${platform_httproot}/RPMs/libX11-devel-1.3-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.3-1.fc12",
-                    require => Package["pkgconfig"];
-                "libXau-devel":
-                    source => "${platform_httproot}/RPMs/libXau-devel-1.0.5-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.0.5-1.fc12",
-                    require => Package["libX11-devel"];
-                "libXdamage-devel":
-                    source => "${platform_httproot}/RPMs/libXdamage-devel-1.1.2-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.1.2-1.fc12",
-                    require => Package["libX11-devel"];
-                "libXdmcp-devel":
-                    source => "${platform_httproot}/RPMs/libXdmcp-devel-1.0.3-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.0.3-1.fc12",
-                    require => Package["libX11-devel"];
-                "libXext-devel":
-                    source => "${platform_httproot}/RPMs/libXext-devel-1.1-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.1-1.fc12",
-                    require => Package["libX11-devel"];
-                "libXfixes-devel":
-                    source => "${platform_httproot}/RPMs/libXfixes-devel-4.0.4-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "4.0.4-1.fc12",
-                    require => Package["libX11-devel"];
-                "libXxf86vm-devel":
-                    source => "${platform_httproot}/RPMs/libXxf86vm-devel-1.1.0-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.1.0-1.fc12",
-                    require => Package["libX11-devel"];
-                "libdrm-devel":
-                    source => "${platform_httproot}/RPMs/libdrm-devel-2.4.15-4.fc12.${hardwaremodel}.rpm",
-                    ensure => "2.4.15-4.fc12";
-                "libxcb-devel":
-                    source => "${platform_httproot}/RPMs/libxcb-devel-1.4-1.fc12.${hardwaremodel}.rpm",
-                    ensure => "1.4-1.fc12";
-                "xorg-x11-proto-devel":
-                    source => "${platform_httproot}/RPMs/xorg-x11-proto-devel-7.4-34.fc12.noarch.rpm",
-                    ensure => "7.4-34.fc12",
-                    require => Package["libX11-devel"];
-                "mesa-libGL-devel":
-                    source => "${platform_httproot}/RPMs/mesa-libGL-devel-7.6-0.13.fc12.${hardwaremodel}.rpm",
-                    ensure => '7.6-0.13.fc12',
-                    require => Package["kernel-headers"],
-                    require => Package["libX11-devel"],
-                    require => Package["libXau-devel"],
-                    require => Package["libXdamage-devel"],
-                    require => Package["libXdmcp-devel"],
-                    require => Package["libXext-devel"],
-                    require => Package["libXfixes-devel"],
-                    require => Package["libXxf86vm-devel"],
-                    require => Package["libdrm-devel"],
-                    require => Package["libxcb-devel"],
-                    require => Package["xorg-x11-proto-devel"];
-            }
-        }
-    }
-
+    
     # NetworkManager kinda sucks, so disable it
     service {
         "network":
