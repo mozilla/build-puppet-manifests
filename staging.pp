cc988f64066e486efde07246b9d23697cf76d1cc
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-03-13 13:07:13 -0400

    Bug 848885. Remove these staging machines. r=nthomas

--- staging.pp
+++ staging.pp
@@ -42,15 +42,3 @@ node "talos-r4-snow-046" inherits "darwin10-i386-test" {
 node "talos-r3-fed-001" inherits "fedora12-i686-test" {
     include talosslave
 }
-
-node "talos-r3-fed-002" inherits "fedora12-i686-test" {
-    include talosslave
-}
-
-node "talos-r3-fed64-010" inherits "fedora12-x86_64-test" {
-    include talosslave
-}
-
-node "talos-r4-lion-001" inherits "darwin11-x86_64-test" {
-    include talos_osx_rev4
-}
