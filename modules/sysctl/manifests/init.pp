108d3607169547bd4a49422488ac6897b97d7c2d
Rail Aliiev <rail@mozilla.com>
2013-04-04 16:16:32 -0400

    No bug. Use "grep -w" to match word boundares and suppress warnings. r=catlee

--- modules/sysctl/manifests/init.pp
+++ modules/sysctl/manifests/init.pp
@@ -2,6 +2,6 @@ define sysctl($value) {
     exec {
         "sysctl-$name":
             command => "/sbin/sysctl $name=$value",
-            onlyif  => "/sbin/sysctl -n $name | grep -q -v '\<$value\>'";
+            onlyif  => "/sbin/sysctl -n $name | grep -q -v -w '$value'";
     }
 }
